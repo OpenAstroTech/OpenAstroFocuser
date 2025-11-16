a63bb9140c4d9f690a533ccab5618750a2a1243c
Andre Stefanov <mail@andrestefanov.de>
2025-11-16 17:54:01 +0100

    refactor: Remove FirmwareHandler files and update error messages for renaming to Focuser

--- app/src/FirmwareHandler.cpp
+++
@@ -1,1 +0,0 @@
-#error "FirmwareHandler.cpp has been renamed to Focuser.cpp. Please update your build to use src/Focuser.cpp."
--- app/src/FirmwareHandler.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#error "FirmwareHandler.hpp has been renamed to Focuser.hpp. Include Focuser.hpp instead."
